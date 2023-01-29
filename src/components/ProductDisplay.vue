b1503f1f34a44dcff19d2ccf29f684278b07b107
HyuuuRiie <wahyuseptiadifajri11@gmail.com>
2023-01-29 15:59:11 +0700

    first commit

---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    v-bind:class="{
+      'page-men': category === 'men’s clothing',
+      'page-women': category === 'women’s clothing',
+      'page-unavailable':
+        category !== 'men’s clothing' && category !== 'women’s clothing',
+    }"
+  >
+    <!-- konten section -->
+    <div class="page-men">
+      <div class="container">
+        <div class="card">
+          <div class="img-card">
+            <img src="../assets/1.jpg" alt="gambar" class="img">
+          </div>
+          <div class="title">Men Casual Slim Fit</div>
+          <div class="category">men's clothing</div>
+          <div class="border"></div>
+          <div class="description">great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.</div>
+          <div class="border2"></div>
+          <div class="price">$29.95</div>
+          <button class="buy">Buy Now</button>
+          <button class="next">Next Product</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductDisplay",
+  
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <ProductDisplay/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProductDisplay from '@/components/ProductDisplay.vue'
+import axios from "axios";
+
+export default {
+  name: 'HomeView',
+  components: {
+    ProductDisplay
+  },
+  data() {
+    return {
+      mens: [],
+    };
+  },
+  // data yang diambil dari json dimasukkan dalam product
+  methods: {
+    setProduct(data) {
+      this.mens = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/mens")
+      .then((response) =>
+        // handle success
+        this.setProduct(response.data))
+      .catch((error) => console.log(error));
+  },
+}
+</script>
